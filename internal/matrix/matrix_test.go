5b9fb95bc48145d5b977256f72504289a886bb34
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2020-12-30 17:30:32 -0500

    Update testing to use subtests

--- internal/matrix/matrix_test.go
+++ internal/matrix/matrix_test.go
@@ -9,57 +9,76 @@ import (
 
 func TestBasicString(t *testing.T) {
 	tests := []struct {
+		name       string
 		rows, cols int
 		expected   string
 	}{
 		{
-			1, 1, "0",
+			"1x1", 1, 1, "0",
 		},
 		{
-			3, 1, "0\n0\n0",
+			"3x1", 3, 1, "0\n0\n0",
 		},
 		{
-			3, 3, "0 0 0\n0 0 0\n0 0 0",
+			"3x3", 3, 3, "0 0 0\n0 0 0\n0 0 0",
 		},
 		{
-			5, 5, "0 0 0 0 0\n0 0 0 0 0\n0 0 0 0 0\n0 0 0 0 0\n0 0 0 0 ",
+			"5x5", 5, 5, "0 0 0 0 0\n0 0 0 0 0\n0 0 0 0 0\n0 0 0 0 0\n0 0 0 0 0",
 		},
 	}
 	for _, test := range tests {
-		m := matrix.NewBasic(test.rows, test.cols)
-		mString := fmt.Sprint(m)
-		if mString != test.expected {
-			t.Errorf(
-				"expected a %dx%d matrix to produce the following string:\n\n"+
-					"%s\n\ninstead it produced:\n\n%s",
-				test.rows,
-				test.cols,
-				mString,
-				test.expected,
-			)
-		}
+		t.Run(test.name, func(t *testing.T) {
+			m := matrix.NewBasic(test.rows, test.cols)
+			mString := fmt.Sprint(m)
+			if mString != test.expected {
+				t.Fatalf(
+					"expected a %dx%d matrix to produce the following string:\n\n"+
+						"%s\n\ninstead it produced:\n\n%s",
+					test.rows,
+					test.cols,
+					mString,
+					test.expected,
+				)
+			}
+		})
 	}
 }
 
-func TestNewBasicFromSlice1D() {
+func TestNewBasicFromSlice(t *testing.T) {
 	tests := []struct {
+		name       string
 		rows, cols int
 		data       []float64
 	}{
 		{
-			1, 1, []float64{15},
+			"1x1", 1, 1, []float64{15},
 		},
 		{
-			3, 1, []float64{15, 64, 32},
+			"3x1 Vector", 3, 1, []float64{15, 64, 32},
 		},
 		{
-			3, 3, []float64{1, 3, 9, 2, 4, 6, 7, 14, 21},
+			"3x3 Matrix", 3, 3, []float64{1, 3, 9, 2, 4, 6, 7, 14, 21},
 		},
 	}
 	for _, test := range tests {
-		m := matrix.NewBasicFromSlice(test.data, test.rows, test.cols)
-		for i, v := range test.data {
-			if m.Get(i/row, i%row)
-		}
+		t.Run(test.name, func(t *testing.T) {
+			m := matrix.NewBasicFromSlice(test.data, test.rows, test.cols)
+			for i, v := range test.data {
+				row := i / test.rows
+				col := i % test.rows
+				if v != m.Get(row, col) {
+					t.Fatalf(
+						"At row %d, col %d the matrix was expected to return %f"+
+							"as prescribed in the test data (%v). Instead it "+
+							"returned %f",
+						row,
+						col,
+						v,
+						test.data,
+						m.Get(row, col),
+					)
+				}
+			}
+		})
 	}
 }
